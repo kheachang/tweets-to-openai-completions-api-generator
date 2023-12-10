55cd27cf295ccc4a54569beb6b4d25d3683b0709
Khea Chang <kheacyc@gmail.com>
2023-12-09 18:28:22 -0800

    modify to create the json file

--- tweets_to_completions.py
+++ tweets_to_completions.py
@@ -1,11 +1,11 @@
 import csv
 import json
+import os
 
 CSV_FILEPATH = r'tweets/tweets.csv'
-JSON_FILEPATH = r'tweets/tweets.json'
 
-def find_date_column(CSV_FILEPATH):
-    with open(CSV_FILEPATH, encoding='utf-8-sig') as csvf:
+def find_date_column(csvFilePath):
+    with open(csvFilePath, encoding='utf-8-sig') as csvf:
         csvReader = csv.DictReader(csvf)
         if '\ufeff"Date"' in csvReader.fieldnames:
             return '\ufeff"Date"'  # Return the correct header name if found
@@ -14,26 +14,28 @@ def find_date_column(CSV_FILEPATH):
         else:
             return None  # Return None if the column header is not found
 
-def make_json(CSV_FILEPATH, JSON_FILEPATH, num_of_rows):
+def make_json(csvFilePath, num_rows=50):  # Accept num_rows as an argument
     data = {}
-    date_column = find_date_column(CSV_FILEPATH)  # Get the date column header
+    date_column = find_date_column(csvFilePath)  # Get the date column header
 
     if date_column:
-        with open(CSV_FILEPATH, encoding='utf-8-sig') as csvf:
+        json_file_name = f"tweets_{num_rows}_rows.json"  # Create JSON file name dynamically
+        jsonFilePath = os.path.join("tweets", json_file_name)  # Construct the JSON file path
+        with open(csvFilePath, encoding='utf-8-sig') as csvf:
             csvReader = csv.DictReader(csvf)
             row_count = 0  # Initialize row counter
             for rows in csvReader:
                 key = rows[date_column]  # Use the identified date column header
                 data[key] = rows
                 row_count += 1
-                if row_count >= num_of_rows:  # Check if the specified number of rows is reached
+                if row_count >= num_rows:  # Check if the specified number of rows is reached
                     break  # Exit the loop if the specified number of rows is processed
-        with open(JSON_FILEPATH, 'w', encoding='utf-8') as jsonf:
+
+        with open(jsonFilePath, 'w', encoding='utf-8') as jsonf:
             jsonf.write(json.dumps(data, indent=4))
 
-        print(f"JSON file '{JSON_FILEPATH}' created successfully.")
+        print(f"JSON file '{json_file_name}' created successfully at '{jsonFilePath}'.")
     else:
         print("No 'Date' column found in the CSV file.")
-		
 
-make_json(CSV_FILEPATH, JSON_FILEPATH, 50)
+make_json(CSV_FILEPATH, num_rows=50)  # Call make_json with the specified number of rows (50 in this case)
